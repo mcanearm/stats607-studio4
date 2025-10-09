5e7953341a38268600b06bcf6cd54aa22bed30e7
Matt McAnear <mmcanear@umich.edu>
2025-10-09 12:41:33 -0400

    add fun little print method for the simulation results

--- studio7/src/estimators.py
+++ studio7/src/estimators.py
@@ -23,12 +23,26 @@ class SimulationResult:
         self.predictions = np.array([res["predictions"] for res in result_set])
 
     def __str__(self):
-        out_text = f"""Sim Result: {self.name}\nN_sim: {len(self.r2)}\nBetas_CI: 
-            {np.percentile(self.betas, [2.5, 97.5], axis=0)}"
-        """
-            # RMSE_CI: {np.percentile(self.rmse, 2.5):0.3f} - {np.percentile(self.rmse, 97.5):0.3f}
-            # R2_CI: {np.percentile(self.r2, 2.5):0.3f} - {np.percentile(self.r2, 97.5):0.3f}
-        return textwrap.dedent(out_text)
+        rmse_ci_str = f"RMSE: {self._ci_string(self.rmse)}"
+        r2_ci_str = f"R2: {self._ci_string(self.r2)}"
+        betas_ci_str = [
+            f"\tBeta {i+1}: {self._ci_string(self.betas[:, i])}"
+            for i in range(self.betas.shape[1])
+        ]
+        start_text = (
+            f"{'-'*40}\nSim Result: {self.name}\nN_sim: {len(self.r2)}"
+        )
+        str_components = [start_text, "Betas:", *betas_ci_str, rmse_ci_str, r2_ci_str] 
+        text_out = "\n".join(str_components)
+        text_out += f"\n{'-'*40}"
+            
+        # RMSE_CI: {np.percentile(self.rmse, 2.5):0.3f} - {np.percentile(self.rmse, 97.5):0.3f}
+        # R2_CI: {np.percentile(self.r2, 2.5):0.3f} - {np.percentile(self.r2, 97.5):0.3f}
+        return textwrap.dedent(text_out)
+
+    @staticmethod
+    def _ci_string(v):
+        return f"({np.percentile(v, 2.5):0.3f}, {np.percentile(v, 97.5):0.3f})"
 
 
 def run_simulation(estimator_class, n_sim=1000, **data_params):
@@ -47,5 +61,4 @@ def run_simulation(estimator_class, n_sim=1000, **data_params):
         r2 = r2_score(y, preds)
         return {"predictions": preds, "beta_hat": beta_hat, "rmse": rmse, "r2": r2}
 
-
-print(run_simulation(LinearRegression, n_sim=10))
+print(run_simulation(LinearRegression, n_sim=100))
